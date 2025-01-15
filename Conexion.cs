0357896ab73171fb3a51c6bd3551e6ece10b30f7
Yorka Munchuli <ymunchuli@gmail.com>
2025-01-15 14:26:08 -0400

    Conexion de la base de datos al c#.

---
+++ Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net;
+
+public class Class1
+{
+	public void conectar()
+	{
+        string nombre_servidor = Dns.GetHostName();
+        string connectionString = "Data Source="+nombre_servidor+"\\SQLEXPRESS;Initial Catalog=db_prestamos;Integrated Security=True;Trust Server Certificate=True";
+
+    }
+}
