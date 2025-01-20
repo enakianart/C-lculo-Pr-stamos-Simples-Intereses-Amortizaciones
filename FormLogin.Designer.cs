36410bd63a01b4101ca8622326710ddd0c7a27e5
Kianna Pascual <kiannalorenapascual@gmail.com>
2025-01-20 15:00:42 -0400

    botones en el form login (parte visual)

--- FormLogin.Designer.cs
+++ FormLogin.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClienteBTN = new System.Windows.Forms.Button();
+            this.PrestamosBTN = new System.Windows.Forms.Button();
+            this.ReportesBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Cálculo_Préstamos_Simples__Intereses__Amortizaciones.Properties.Resources.Hiki_nivel_1_muy_feli;
+            this.pictureBox1.Location = new System.Drawing.Point(410, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(333, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 70);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ranita\'s Bank";
+            // 
+            // ClienteBTN
+            // 
+            this.ClienteBTN.BackColor = System.Drawing.Color.White;
+            this.ClienteBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClienteBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ClienteBTN.Location = new System.Drawing.Point(18, 47);
+            this.ClienteBTN.Name = "ClienteBTN";
+            this.ClienteBTN.Size = new System.Drawing.Size(216, 54);
+            this.ClienteBTN.TabIndex = 4;
+            this.ClienteBTN.Text = "Cliente";
+            this.ClienteBTN.UseVisualStyleBackColor = false;
+            // 
+            // PrestamosBTN
+            // 
+            this.PrestamosBTN.BackColor = System.Drawing.Color.White;
+            this.PrestamosBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrestamosBTN.Location = new System.Drawing.Point(18, 183);
+            this.PrestamosBTN.Name = "PrestamosBTN";
+            this.PrestamosBTN.Size = new System.Drawing.Size(216, 54);
+            this.PrestamosBTN.TabIndex = 5;
+            this.PrestamosBTN.Text = "Préstamos";
+            this.PrestamosBTN.UseVisualStyleBackColor = false;
+            // 
+            // ReportesBTN
+            // 
+            this.ReportesBTN.BackColor = System.Drawing.Color.White;
+            this.ReportesBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReportesBTN.Location = new System.Drawing.Point(18, 335);
+            this.ReportesBTN.Name = "ReportesBTN";
+            this.ReportesBTN.Size = new System.Drawing.Size(216, 54);
+            this.ReportesBTN.TabIndex = 6;
+            this.ReportesBTN.Text = "Reportes";
+            this.ReportesBTN.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(316, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(457, 72);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Consulta y registro de clientes,\r\n préstamos, repostes y amortización.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Turquoise;
+            this.panel1.Controls.Add(this.ClienteBTN);
+            this.panel1.Controls.Add(this.PrestamosBTN);
+            this.panel1.Controls.Add(this.ReportesBTN);
+            this.panel1.Location = new System.Drawing.Point(12, 11);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(253, 475);
+            this.panel1.TabIndex = 8;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(836, 498);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormLogin";
             this.Text = "FormLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClienteBTN;
+        private System.Windows.Forms.Button PrestamosBTN;
+        private System.Windows.Forms.Button ReportesBTN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
