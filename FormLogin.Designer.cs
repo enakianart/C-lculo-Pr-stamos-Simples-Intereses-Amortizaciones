d98ddc8c4cfcbe6f2618e756717d1aa5c7da1511
Yorka Munchuli <ymunchuli@gmail.com>
2025-01-26 23:08:49 -0400

    Paquete de ReportViewer instalado.

--- FormLogin.Designer.cs
+++ FormLogin.Designer.cs
@@ -35,8 +35,8 @@
             this.ReportesBTN = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.PagosBTN = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -46,9 +46,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(333, 32);
+            this.label1.Location = new System.Drawing.Point(222, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(397, 70);
+            this.label1.Size = new System.Drawing.Size(265, 44);
             this.label1.TabIndex = 3;
             this.label1.Text = "Ranita\'s Bank";
             // 
@@ -57,9 +58,10 @@
             this.ClienteBTN.BackColor = System.Drawing.Color.White;
             this.ClienteBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ClienteBTN.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.ClienteBTN.Location = new System.Drawing.Point(18, 47);
+            this.ClienteBTN.Location = new System.Drawing.Point(12, 31);
+            this.ClienteBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ClienteBTN.Name = "ClienteBTN";
-            this.ClienteBTN.Size = new System.Drawing.Size(216, 54);
+            this.ClienteBTN.Size = new System.Drawing.Size(144, 35);
             this.ClienteBTN.TabIndex = 4;
             this.ClienteBTN.Text = "Cliente";
             this.ClienteBTN.UseVisualStyleBackColor = false;
@@ -68,9 +70,10 @@
             // 
             this.PrestamosBTN.BackColor = System.Drawing.Color.White;
             this.PrestamosBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PrestamosBTN.Location = new System.Drawing.Point(18, 146);
+            this.PrestamosBTN.Location = new System.Drawing.Point(12, 95);
+            this.PrestamosBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PrestamosBTN.Name = "PrestamosBTN";
-            this.PrestamosBTN.Size = new System.Drawing.Size(216, 54);
+            this.PrestamosBTN.Size = new System.Drawing.Size(144, 35);
             this.PrestamosBTN.TabIndex = 5;
             this.PrestamosBTN.Text = "Préstamos";
             this.PrestamosBTN.UseVisualStyleBackColor = false;
@@ -79,9 +82,10 @@
             // 
             this.ReportesBTN.BackColor = System.Drawing.Color.White;
             this.ReportesBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ReportesBTN.Location = new System.Drawing.Point(18, 335);
+            this.ReportesBTN.Location = new System.Drawing.Point(12, 218);
+            this.ReportesBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ReportesBTN.Name = "ReportesBTN";
-            this.ReportesBTN.Size = new System.Drawing.Size(216, 54);
+            this.ReportesBTN.Size = new System.Drawing.Size(144, 35);
             this.ReportesBTN.TabIndex = 6;
             this.ReportesBTN.Text = "Reportes";
             this.ReportesBTN.UseVisualStyleBackColor = false;
@@ -91,9 +95,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(316, 385);
+            this.label2.Location = new System.Drawing.Point(211, 250);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(457, 72);
+            this.label2.Size = new System.Drawing.Size(311, 48);
             this.label2.TabIndex = 7;
             this.label2.Text = "Consulta y registro de clientes,\r\n préstamos, repostes y amortización.";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -105,42 +110,46 @@
             this.panel1.Controls.Add(this.ClienteBTN);
             this.panel1.Controls.Add(this.PrestamosBTN);
             this.panel1.Controls.Add(this.ReportesBTN);
-            this.panel1.Location = new System.Drawing.Point(12, 11);
+            this.panel1.Location = new System.Drawing.Point(8, 7);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(253, 475);
+            this.panel1.Size = new System.Drawing.Size(169, 309);
             this.panel1.TabIndex = 8;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(410, 127);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(257, 216);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // PagosBTN
             // 
             this.PagosBTN.BackColor = System.Drawing.Color.White;
             this.PagosBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PagosBTN.Location = new System.Drawing.Point(18, 239);
+            this.PagosBTN.Location = new System.Drawing.Point(12, 155);
+            this.PagosBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PagosBTN.Name = "PagosBTN";
-            this.PagosBTN.Size = new System.Drawing.Size(216, 54);
+            this.PagosBTN.Size = new System.Drawing.Size(144, 35);
             this.PagosBTN.TabIndex = 7;
             this.PagosBTN.Text = "Pagos";
             this.PagosBTN.UseVisualStyleBackColor = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(273, 83);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Purple;
-            this.ClientSize = new System.Drawing.Size(836, 498);
+            this.ClientSize = new System.Drawing.Size(557, 324);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormLogin";
             this.Text = "FormLogin";
             this.panel1.ResumeLayout(false);
