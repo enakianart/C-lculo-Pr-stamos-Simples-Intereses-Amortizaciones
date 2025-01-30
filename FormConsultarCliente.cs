525c0a862da59ed488634caa38c08c29a4e4fe43
Kianna Pascual <kiannalorenapascual@gmail.com>
2025-01-30 07:29:13 -0400

    Arreglar problema

---
+++ FormConsultarCliente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cálculo_Préstamos_Simples__Intereses__Amortizaciones
+{
+    public partial class FormConsultarCliente : Form
+    {
+        string nombre_servidor = Dns.GetHostName();
+        public string conectar = "Data Source=ENZOACER\\SQLEXPRESS;Initial Catalog=db_prestamos;Integrated Security=True;";
+
+        public FormConsultarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void VolverBTN_Click(object sender, EventArgs e)
+        {
+            FormularioClientes formularioClientes = new FormularioClientes();
+            formularioClientes.Show();
+            this.Close();
+        }
+
+        private void BuscarBTN_Click(object sender, EventArgs e)
+        {
+
+            string buscar = Buscartxt.Text;
+            using (SqlConnection conexion = new SqlConnection(conectar))
+            {
+                conexion.Open();
+                string consulta = $"SELECT * FROM Clientes WHERE Nombre Like '{buscar}%'";
+                SqlCommand comando = new SqlCommand(consulta,conexion);
+                SqlDataAdapter adaptador = new SqlDataAdapter(comando);
+                DataTable tablaclientes = new DataTable();
+                adaptador.Fill(tablaclientes);
+                dataGridView1.DataSource = tablaclientes;
+
+
+                dataGridView1.Columns["ClientesId"].ReadOnly = true;
+                dataGridView1.Columns["Nombre"].ReadOnly = false; 
+                dataGridView1.Columns["Telefono"].ReadOnly = false;
+                dataGridView1.Columns["Direccion"].ReadOnly = false;
+                dataGridView1.Columns["Sueldo"].ReadOnly = true;
+                dataGridView1.Columns["Garantia"].ReadOnly = true;
+
+                dataGridView1.AllowUserToDeleteRows = false;
+
+            }
+        }
+        
+        }
+    }
+
+    
---
+++ FormularioClientes.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Cálculo_Préstamos_Simples__Intereses__Amortizaciones
+{
+    partial class FormularioClientes
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConsultarClienteBTN = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GestionarClienteBTN = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.VolverBTN = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConsultarClienteBTN
+            // 
+            this.ConsultarClienteBTN.BackColor = System.Drawing.Color.White;
+            this.ConsultarClienteBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConsultarClienteBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ConsultarClienteBTN.Location = new System.Drawing.Point(50, 36);
+            this.ConsultarClienteBTN.Name = "ConsultarClienteBTN";
+            this.ConsultarClienteBTN.Size = new System.Drawing.Size(320, 54);
+            this.ConsultarClienteBTN.TabIndex = 4;
+            this.ConsultarClienteBTN.Text = "Consultar cliente";
+            this.ConsultarClienteBTN.UseVisualStyleBackColor = false;
+            this.ConsultarClienteBTN.Click += new System.EventHandler(this.ConsultarClienteBTN_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Turquoise;
+            this.panel1.Controls.Add(this.ConsultarClienteBTN);
+            this.panel1.Controls.Add(this.GestionarClienteBTN);
+            this.panel1.Location = new System.Drawing.Point(12, 316);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(947, 122);
+            this.panel1.TabIndex = 9;
+            // 
+            // GestionarClienteBTN
+            // 
+            this.GestionarClienteBTN.BackColor = System.Drawing.Color.White;
+            this.GestionarClienteBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GestionarClienteBTN.Location = new System.Drawing.Point(538, 36);
+            this.GestionarClienteBTN.Name = "GestionarClienteBTN";
+            this.GestionarClienteBTN.Size = new System.Drawing.Size(341, 54);
+            this.GestionarClienteBTN.TabIndex = 5;
+            this.GestionarClienteBTN.Text = "Gestionar cliente";
+            this.GestionarClienteBTN.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Cálculo_Préstamos_Simples__Intereses__Amortizaciones.Properties.Resources.Hiki_nivel_1_muy_feli;
+            this.pictureBox1.Location = new System.Drawing.Point(359, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(368, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 70);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Clientes";
+            // 
+            // VolverBTN
+            // 
+            this.VolverBTN.BackColor = System.Drawing.Color.White;
+            this.VolverBTN.Font = new System.Drawing.Font("Franklin Gothic Medium", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VolverBTN.Location = new System.Drawing.Point(835, 260);
+            this.VolverBTN.Name = "VolverBTN";
+            this.VolverBTN.Size = new System.Drawing.Size(124, 50);
+            this.VolverBTN.TabIndex = 6;
+            this.VolverBTN.Text = "Volver";
+            this.VolverBTN.UseVisualStyleBackColor = false;
+            this.VolverBTN.Click += new System.EventHandler(this.VolverBTN_Click);
+            // 
+            // FormularioClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(971, 450);
+            this.Controls.Add(this.VolverBTN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormularioClientes";
+            this.Text = "FormularioClientes1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConsultarClienteBTN;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button GestionarClienteBTN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button VolverBTN;
+    }
+}
