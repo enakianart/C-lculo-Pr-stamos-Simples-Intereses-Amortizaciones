56872a031c623a9ab3c7a79eb13471d69e9d3cb3
Kianna Pascual <kiannalorenapascual@gmail.com>
2025-02-02 12:00:23 -0400

    creacion del formulario gestionar pagos

---
+++ FormularioGestionarPago.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cálculo_Préstamos_Simples__Intereses__Amortizaciones
+{
+    public partial class FormularioGestionarPago : Form
+    {
+        public FormularioGestionarPago()
+        {
+            InitializeComponent();
+        }
+
+        private void VolverBTN_Click(object sender, EventArgs e)
+        {
+            FormularioPagos formularioPagos = new FormularioPagos();
+            formularioPagos.Show();
+        }
+    }
+}
